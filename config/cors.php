9379dd4eac1544f8f2c5e84356cb3901bf774c4f
npmFullStack <occ.mangorangca.norway@gmail.com>
2025-04-18 17:41:22 +0800

    Updated

--- config/cors.php
+++ config/cors.php
@@ -17,10 +17,8 @@ return [
 
 'paths' => ['api/*', 'sanctum/csrf-cookie'],
 'allowed_methods' => ['*'],
-'allowed_origins' => ['*'], // Or specify your frontend URL
+'allowed_origins' => ['https://pradduct-frontend.vercel.app'],
 'allowed_headers' => ['*'],
-'exposed_headers' => [],
-'max_age' => 0,
 'supports_credentials' => true,
 
 ];
